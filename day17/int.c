d7962efba5645b580ac86cacaf34a624a06ad96d
Anjiansan <anjiansan@gmail.com>
2017-03-04 23:12:05 +0800

    Add day17

---
+++ day17/int.c
@@ -0,0 +1,29 @@
+#include "bootpack.h"
+
+//PIC初始化
+void init_pic(void)
+{
+    io_out8(PIC0_IMR,0xFF);//禁止所有中断
+    io_out8(PIC1_IMR,0xFF);//禁止所有中断
+
+    io_out8(PIC0_ICW1,0x11);//边沿触发模式
+    io_out8(PIC0_ICW2,0x20);//IRQ0-7由INT20-27接受
+    io_out8(PIC0_ICW3,1 << 2);//PIC1由IRQ2接受
+    io_out8(PIC0_ICW4,0x01);//无缓冲模式
+
+    io_out8(PIC1_ICW1,0x11);//边沿触发模式
+    io_out8(PIC1_ICW2,0x28);//IRQ8-15由INT28-2F接受
+    io_out8(PIC1_ICW3,2);//PIC1由IRQ2接受
+    io_out8(PIC1_ICW4,0x01);//无缓冲模式
+
+    io_out8(PIC0_IMR,0xFB);//PIC1以外全部禁止
+    io_out8(PIC1_IMR,0xFF);//禁止所有中断
+
+    return;
+}
+
+void inthandler27(int *esp)
+{
+    io_out8(PIC0_OCW2,0x67);
+    return;
+}
